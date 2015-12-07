b61a291f3ac10970d73306b33ce71d0eb38efe48
Neil <neil@Neils-MacBook-Pro.local>
2015-12-07 11:49:34 -0800

    Adding JS-Protractor-Selenium test framework

---
+++ README.md
@@ -0,0 +1,15 @@
+# Changes to be made (reference Sauce Account)
+```
+	export SAUCE_USERNAME=your_username
+	export SAUCE_ACCESS_KEY=your_access_key
+```
+
+# Setup
+```
+	npm install
+```
+
+# Running tests
+```
+	./node_modules/.bin/protractor conf.js
+```
---
+++ conf.js
@@ -0,0 +1,40 @@
+// conf.js
+exports.config = {
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+  //seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
+  specs: ['specs/*spec.js'],
+
+  restartBrowserBetweenTests: true,
+
+  onPrepare: function(){
+      var caps = browser.getCapabilities()
+  },
+
+  multiCapabilities: [{
+    browserName: 'firefox',
+    version: '32',
+    platform: 'OS X 10.10',
+    name: "firefox-tests",
+    shardTestFiles: true,
+    maxInstances: 25
+  }, {
+    browserName: 'chrome',
+    version: '41',
+    platform: 'Windows 7',
+    name: "chrome-tests",
+    shardTestFiles: true,
+    maxInstances: 25
+  }],
+
+  onComplete: function() {
+
+    var printSessionId = function(jobName){
+      browser.getSession().then(function(session) {
+        console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+      });
+    }
+    printSessionId("Insert Job Name Here");
+  }
+}
