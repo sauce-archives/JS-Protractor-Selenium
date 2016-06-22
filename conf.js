2054ba35349b32a36fd72d45de08a661d20d6c18
Mehmet Gerceker <mehmetg@msn.com>
2016-06-22 11:25:12 -0700

    Adding proxy config for REST and Webdriver connections. Will not run with a valid proxy.

--- conf.js
+++ conf.js
@@ -1,40 +1,46 @@
 // conf.js
+var HTTPSProxyAgent = require('https-proxy-agent');
+var sauceRestAgent = new HTTPSProxyAgent("http://<proxy>:<port>")
+
 exports.config = {
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+    sauceAgent: sauceRestAgent,
 
-  //seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
-  specs: ['specs/*spec.js'],
+    webDriverProxy: 'http://<proxy>:<port>',
 
-  // restartBrowserBetweenTests: true,
+    //seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
+    specs: ['specs/*spec.js'],
 
-  onPrepare: function(){
-      var caps = browser.getCapabilities()
-  },
+    // restartBrowserBetweenTests: true,
 
-  multiCapabilities: [{
-    browserName: 'firefox',
-    version: '32',
-    platform: 'OS X 10.10',
-    name: "firefox-tests",
-    shardTestFiles: true,
-    maxInstances: 25
-  }, {
-    browserName: 'chrome',
-    version: '41',
-    platform: 'Windows 7',
-    name: "chrome-tests",
-    shardTestFiles: true,
-    maxInstances: 25
-  }],
+    onPrepare: function () {
+        var caps = browser.getCapabilities()
+    },
 
-  onComplete: function() {
+    multiCapabilities: [{
+        browserName: 'firefox',
+        version: '32',
+        platform: 'OS X 10.10',
+        name: "firefox-tests",
+        shardTestFiles: true,
+        maxInstances: 25
+    }, {
+        browserName: 'chrome',
+        version: '41',
+        platform: 'Windows 7',
+        name: "chrome-tests",
+        shardTestFiles: true,
+        maxInstances: 25
+    }],
 
-    var printSessionId = function(jobName){
-      browser.getSession().then(function(session) {
-        console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
-      });
+    onComplete: function () {
+
+        var printSessionId = function (jobName) {
+            browser.getSession().then(function (session) {
+                console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+            });
+        }
+        printSessionId("Insert Job Name Here");
     }
-    printSessionId("Insert Job Name Here");
-  }
 }
