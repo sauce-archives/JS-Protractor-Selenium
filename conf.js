e122b74e0543e4982c8fed608d0588f697f299f2
joshgrant <joshua.m.grant@gmail.com>
2019-03-18 16:14:11 -0400

    removed some tests

--- conf.js
+++ conf.js
@@ -10,25 +10,21 @@ exports.config = {
     // webDriverProxy: 'http://<proxy>:<port>',
 
     //seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
-    specs: ['specs/*spec.js'],
+    specs: ['specs/**/*spec.js'],
 
     // restartBrowserBetweenTests: true,
-
-    onPrepare: function () {
-        var caps = browser.getCapabilities()
-    },
 
     multiCapabilities: [{
         browserName: 'firefox',
         version: 'latest',
-        platform: 'OS X 10.10',
+        platform: 'OS X 10.13',
         name: "firefox-tests",
         shardTestFiles: true,
         maxInstances: 25
     }, {
         browserName: 'chrome',
-        version: '41',
-        platform: 'Windows 7',
+        version: '72',
+        platform: 'Windows 10',
         name: "chrome-tests",
         shardTestFiles: true,
         maxInstances: 25
